816a185ccb50104f7d8cb714a173b0789094e4cb
Ihtisham-Khattak <ikpashteen1@gmail.com>
2023-11-12 01:11:47 +0500

    changes pushes to main brach

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className='color-[#088000] text-4xl bg-slate-500'>Hello Sass</h1>
+    <h1 className='text-[#088000] text-4xl bg-slate-500'>Hello Sass</h1>
   )
 }
